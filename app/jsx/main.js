a2dddc70375023665b08675f19d10b38fca04d76
Damien Ragoucy <damien.ragoucy@af83.com>
2014-05-13 15:58:34 +0200

    Main page: fake user response for user page
    Send main page page as current page props

--- app/jsx/main.js
+++ app/jsx/main.js
@@ -1,7 +1,39 @@
 /** @jsx React.DOM */
 
+var USER = {
+  "login": "AF83",
+  "id": 95956,
+  "avatar_url": "https://avatars.githubusercontent.com/u/95956?",
+  "gravatar_id": "992d03c980132c2e7a88ce144f1a6600",
+  "url": "https://api.github.com/users/AF83",
+  "html_url": "https://github.com/AF83",
+  "followers_url": "https://api.github.com/users/AF83/followers",
+  "following_url": "https://api.github.com/users/AF83/following{/other_user}",
+  "gists_url": "https://api.github.com/users/AF83/gists{/gist_id}",
+  "starred_url": "https://api.github.com/users/AF83/starred{/owner}{/repo}",
+  "subscriptions_url": "https://api.github.com/users/AF83/subscriptions",
+  "organizations_url": "https://api.github.com/users/AF83/orgs",
+  "repos_url": "https://api.github.com/users/AF83/repos",
+  "events_url": "https://api.github.com/users/AF83/events{/privacy}",
+  "received_events_url": "https://api.github.com/users/AF83/received_events",
+  "type": "Organization",
+  "site_admin": false,
+  "name": "af83",
+  "company": null,
+  "blog": "af83.com",
+  "location": "Paris",
+  "email": null,
+  "hireable": null,
+  "bio": null,
+  "public_repos": 114,
+  "public_gists": 0,
+  "followers": 0,
+  "following": 0,
+  "created_at": "2009-06-16T15:18:41Z",
+  "updated_at": "2014-05-13T10:12:57Z"
+};
+
 var MainPage = React.createClass({
-  KEYS_TO_PASS: ["query", "results"],
 
   getDefaultProps: function() {
     var routes = {
@@ -17,22 +49,16 @@ var MainPage = React.createClass({
   getInitialState: function() {
     return {
       query: "",
-      results: {},
-      route: "search"
+      user: USER,
+      resultsPerPage: 30,
+      route: "user"
     };
   },
 
   render: function() {
-    var self = this;
-
-    function addStateEntry(hash, key) {
-      hash[key] = self.state[key];
-      return hash;
-    }
-
-    return this.props.routes[this.state.route](
-      this.KEYS_TO_PASS.reduce(addStateEntry, {})
-    );
+    return this.props.routes[this.state.route]({
+      data: this.state
+    });
   }
 });
 
